014a3ce5b2d7580a0e0f5b9e59d246bb9e46faef
Omolara5861 <debbiegterra@gmail.com>
2022-10-03 12:48:43 +0100

    feat: :sparkles: rendered the emotions in the right tag

--- script.js
+++ script.js
@@ -67,7 +67,29 @@ function renderEmotionsRadios(cats) {
     const emotions = getEmotionsArray(cats);
         let emotionItems = '';
         for(let emotion of emotions) {
-            emotionItems += `<p>${emotion}</p>`;
+    /*
+    Challenge:
+    1. Swap out `<p>${emotion}</p>` for HTML
+    that will render a radio input for each
+    emotion. Remember to use "type", "id", 
+    "value", and "name" properties on each radio.
+    ("id" and "value" can both be set to the
+    "emotion").
+    2. Remember to give each radio a label.
+    (What property does a label need?)
+    3. Enclose each individual radio input in this div:
+    <div class="radio">
+        **RADIO HERE**
+    </div>
+    */ 
+            // emotionItems += `<p>${emotion}</p>`;
+            emotionItems += 
+            `
+            <div class="radio">
+            <label for="${emotion}">${emotion}</label>
+                <input type="radio" id="${emotion}" value="${emotion}" name="emotions">
+            </div>
+            `;
         }
     emotionsContainer.innerHTML = emotionItems;
 }
